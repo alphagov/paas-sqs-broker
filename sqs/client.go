b22a5d6b23172de17c005e8d88a5fbe861744f41
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-09-08 15:01:12 +0100

    add client interface
    the client interface declares the AWS SDK calls that our provider
    requires and allows us to easily generate fakes with counterfieiter for
    mocking out calls during tests

--- sqs/client.go
+++ sqs/client.go
@@ -3,20 +3,30 @@ package sqs
 import (
 	"encoding/json"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 )
 
 //go:generate counterfeiter -o fakes/fake_sqs_client.go . Client
-type Client interface{}
-
-type Config struct {
-	AWSRegion         string `json:"aws_region"`
-	ResourcePrefix    string `json:"resource_prefix"`
-	IAMUserPath       string `json:"iam_user_path"`
-	DeployEnvironment string `json:"deploy_env"`
-	Timeout           time.Duration
+type Client interface {
+	DescribeStacksWithContext(aws.Context, *cloudformation.DescribeStacksInput, ...request.Option) (*cloudformation.DescribeStacksOutput, error)
+	// DescribeStackEventsWithContext(aws.Context, *cloudformation.DescribeStackEventsInput, ...request.Option) (*cloudformation.DescribeStackEventsOutput, error)
+	CreateStackWithContext(aws.Context, *cloudformation.CreateStackInput, ...request.Option) (*cloudformation.CreateStackOutput, error)
+	UpdateStackWithContext(aws.Context, *cloudformation.UpdateStackInput, ...request.Option) (*cloudformation.UpdateStackOutput, error)
+	DeleteStackWithContext(aws.Context, *cloudformation.DeleteStackInput, ...request.Option) (*cloudformation.DeleteStackOutput, error)
 }
 
-func NewSQSClientConfig(configJSON []byte) (*Config, error) {
+type Config struct {
+	AWSRegion           string `json:"aws_region"`
+	ResourcePrefix      string `json:"resource_prefix"`
+	DeployEnvironment   string `json:"deploy_env"`
+	Timeout             time.Duration
+	PermissionsBoundary string `json:"permissions_boundary"`
+}
+
+func NewConfig(configJSON []byte) (*Config, error) {
 	config := &Config{}
 	err := json.Unmarshal(configJSON, &config)
 	if err != nil {
@@ -25,5 +35,3 @@ func NewSQSClientConfig(configJSON []byte) (*Config, error) {
 
 	return config, nil
 }
-
-type SQSClient struct{}
