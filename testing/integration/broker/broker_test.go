251c9a0672c5c2ca8f3b6ccc8d64c75108c0ae70
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-09-08 14:37:10 +0100

    sqs integration tests
    port the s3 integration tests -> sqs integration tests
    
    integration tests are "opt-in" and require that ENABLE_INTEGRATION_TESTS
    environment variable is set, otherwise they will skip. This allows the
    general unit tests to execute as expected with "go test ./..."
    
    The integration tests require to be run with AWS IAM credenitals in the
    environment (or iam-profile/metadata). You may also optionally accept a
    PERMISSION_BOUNDARY_ARN environment variable which will ensure any IAM
    users created will have the attached boundary (this is useful for
    testing stricter permissions setups but not required).
    
    The integration tests perform a basic flow throug the life cycle of a
    queue:
    
    * Provision
    * Wait
    * Bind
    * Wait
    * Update
    * Wait
    * Use credenitals from binding
    * Unbind
    * Wait
    * Deprovision
    
    since the sqs broker uses async bindings we must also remember to poll
    for state after creating bindings too
    
    we perform an update of a single simple parameter and wait for success,
    as a sanity check that update works, testing the correct operation of
    each of the options is out of scope of this test.
    
    Co-Authored-By: Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
    Co-Authored-By: Alex Monk <alex.monk@digital.cabinet-office.gov.uk>

--- testing/integration/broker/broker_test.go
+++ testing/integration/broker/broker_test.go
@@ -1,49 +1,202 @@
 package broker_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	cfn "github.com/aws/aws-sdk-go/service/cloudformation"
+	awssqs "github.com/aws/aws-sdk-go/service/sqs"
+
+	"code.cloudfoundry.org/lager"
+	"github.com/alphagov/paas-service-broker-base/broker"
+	brokertesting "github.com/alphagov/paas-service-broker-base/testing"
+	"github.com/alphagov/paas-sqs-broker/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/pivotal-cf/brokerapi"
+	"github.com/pivotal-cf/brokerapi/domain"
+	uuid "github.com/satori/go.uuid"
 )
 
 const (
 	ASYNC_ALLOWED = true
 )
 
-type BindingResponse struct {
-	Credentials map[string]interface{} `json:"credentials"`
-}
+var (
+	PollingTimeout = time.Minute * 10
+)
 
 var _ = Describe("Broker", func() {
+	var (
+		instanceID string
+		binding1ID string
+		serviceID  = "uuid-1"
+		planID     = "uuid-2"
+	)
 
 	BeforeEach(func() {
-		// instanceID = uuid.NewV4().String()
+		if os.Getenv("ENABLE_INTEGRATION_TESTS") != "true" {
+			Skip("Skipping integration tests as ENABLE_INTEGRATION_TESTS is not set to 'true'")
+		}
+		instanceID = uuid.NewV4().String()
+		binding1ID = uuid.NewV4().String()
 	})
 
-	It("should return a 410 response when trying to delete a non-existent instance", func() {
-	})
-
-	It("should manage the lifecycle of an SQS bucket", func() {
+	It("should manage the lifecycle of an SQS queue", func() {
 		By("initialising")
-		Expect(true).To(Equal(true))
-		// sqsClientConfig, brokerTester := initialise(*BrokerSuiteData.LocalhostIAMPolicyArn)
+		_, brokerTester := initialise()
 
 		By("Provisioning")
+		provisionValues := brokertesting.RequestBody{
+			ServiceID:        serviceID,
+			PlanID:           planID,
+			OrganizationGUID: "some customer",
+			Parameters: &brokertesting.ConfigurationValues{
+				"messageRetentionPeriod": 60,
+			},
+		}
+		res := brokerTester.Provision(instanceID, provisionValues, ASYNC_ALLOWED)
+		Expect(res.Code).To(Equal(http.StatusAccepted))
 
-		// defer helpers.DeprovisionService(brokerTester, instanceID, serviceID, planID)
+		By("waiting for the provision to succeed")
+		Eventually(func() *httptest.ResponseRecorder {
+			return brokerTester.LastOperation(instanceID, serviceID, planID, sqs.ProvisionOperation)
+		}, PollingTimeout, 5*time.Second).Should(HaveLastOperationState(domain.Succeeded))
+
+		defer DeprovisionService(brokerTester, instanceID, serviceID, planID)
 
 		By("Binding an app")
+		res = brokerTester.Bind(instanceID, binding1ID, brokertesting.RequestBody{
+			ServiceID:        serviceID,
+			PlanID:           planID,
+			OrganizationGUID: "some customer",
+		}, ASYNC_ALLOWED)
+		Expect(res.Code).To(Equal(http.StatusAccepted))
 
-		// defer helpers.Unbind(brokerTester, instanceID, serviceID, planID, binding1ID)
+		defer Unbind(brokerTester, instanceID, serviceID, planID, binding1ID)
+
+		By("waiting for the bind to complete")
+		Eventually(func() *httptest.ResponseRecorder {
+			return brokerTester.LastBindingOperation(instanceID, binding1ID, serviceID, planID, sqs.BindOperation)
+		}, PollingTimeout, 5*time.Second).Should(HaveLastOperationState(domain.Succeeded))
+
+		By("getting the binding")
+		res = brokerTester.GetBinding(instanceID, binding1ID, serviceID, planID)
+		Expect(res.Code).To(Equal(http.StatusOK))
 
 		By("Asserting the credentials returned work for both reading and writing")
+		var ret struct {
+			Credentials struct {
+				AccessKeyID     string
+				SecretAccessKey string
+				QueueURL        string
+				DLQueueURL      string
+				Region          string
+			}
+		}
+		err := json.NewDecoder(res.Result().Body).Decode(&ret)
+		Expect(err).ToNot(HaveOccurred())
 
-		By("Binding an app as a read-only user")
+		sess := session.Must(session.NewSession(&aws.Config{
+			Region:      aws.String(ret.Credentials.Region),
+			Credentials: credentials.NewStaticCredentials(ret.Credentials.AccessKeyID, ret.Credentials.SecretAccessKey, ""),
+		}))
+		sqsClient := awssqs.New(sess)
 
-		// defer helpers.Unbind(brokerTester, instanceID, serviceID, planID, binding2ID)
+		_, err = sqsClient.SendMessage(&awssqs.SendMessageInput{
+			MessageBody: aws.String("Hello World."),
+			QueueUrl:    aws.String(ret.Credentials.QueueURL),
+		})
+		Expect(err).ToNot(HaveOccurred())
 
-		By("Asserting that read-only credentials can read, but fail to write to a file")
+		_, err = sqsClient.ReceiveMessage(&awssqs.ReceiveMessageInput{
+			QueueUrl:            aws.String(ret.Credentials.QueueURL),
+			MaxNumberOfMessages: aws.Int64(10),
+		})
+		Expect(err).ToNot(HaveOccurred())
 
-		By("Asserting the first user's credentials still work for reading and writing")
+		By("updating the message retention period")
+		res = brokerTester.Update(instanceID, brokertesting.RequestBody{
+			ServiceID: serviceID,
+			PlanID:    planID,
+			Parameters: &brokertesting.ConfigurationValues{
+				"messageRetentionPeriod": 120,
+			},
+			PreviousValues: &provisionValues,
+		}, ASYNC_ALLOWED)
+		Expect(res.Code).To(Equal(http.StatusAccepted))
+
+		By("waiting for the update to succeed")
+		Eventually(func() *httptest.ResponseRecorder {
+			return brokerTester.LastOperation(instanceID, serviceID, planID, sqs.UpdateOperation)
+		}, PollingTimeout, 5*time.Second).Should(HaveLastOperationState(domain.Succeeded))
+
 	})
 
 })
+
+func DeprovisionService(brokerTester brokertesting.BrokerTester, instanceID, serviceID, planID string) {
+	By("Deprovisioning")
+	res := brokerTester.Deprovision(instanceID, serviceID, planID, true)
+	Expect(res.Code).To(Equal(http.StatusAccepted))
+
+	By("waiting for the deprovision to succeed")
+	Eventually(func() *httptest.ResponseRecorder {
+		return brokerTester.LastOperation(instanceID, serviceID, planID, sqs.DeprovisionOperation)
+	}, PollingTimeout, 5*time.Second).Should(HaveLastOperationState(domain.Succeeded))
+}
+
+func Unbind(brokerTester brokertesting.BrokerTester, instanceID string, serviceID string, planID string, bindingID string) {
+	By(fmt.Sprintf("Deferred: Unbinding the %s binding", bindingID))
+	res := brokerTester.Unbind(instanceID, serviceID, planID, bindingID, true)
+	Expect(res.Code).To(Equal(http.StatusAccepted))
+
+	By("waiting for the unbind to complete")
+	Eventually(func() *httptest.ResponseRecorder {
+		return brokerTester.LastBindingOperation(instanceID, bindingID, serviceID, planID, sqs.UnbindOperation)
+	}, PollingTimeout, 5*time.Second).Should(HaveLastOperationState(domain.Succeeded))
+}
+
+func initialise() (*sqs.Config, brokertesting.BrokerTester) {
+	file, err := os.Open("../../fixtures/config.json")
+	Expect(err).ToNot(HaveOccurred())
+	defer file.Close()
+
+	config, err := broker.NewConfig(file)
+	Expect(err).ToNot(HaveOccurred())
+
+	sqsClientConfig, err := sqs.NewConfig(config.Provider)
+	Expect(err).ToNot(HaveOccurred())
+
+	logger := lager.NewLogger("sqs-service-broker-test")
+	logger.RegisterSink(lager.NewWriterSink(GinkgoWriter, config.API.LagerLogLevel))
+
+	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(sqsClientConfig.AWSRegion)}))
+
+	sqsProvider := &sqs.Provider{
+		Client:              cfn.New(sess),
+		Environment:         sqsClientConfig.DeployEnvironment,
+		ResourcePrefix:      sqsClientConfig.ResourcePrefix,
+		PermissionsBoundary: sqsClientConfig.PermissionsBoundary,
+		Timeout:             sqsClientConfig.Timeout,
+		Logger:              logger,
+	}
+
+	serviceBroker, err := broker.New(config, sqsProvider, logger)
+	Expect(err).ToNot(HaveOccurred())
+	brokerAPI := broker.NewAPI(serviceBroker, logger, config)
+
+	return sqsClientConfig, brokertesting.New(brokerapi.BrokerCredentials{
+		Username: "username",
+		Password: "password",
+	}, brokerAPI)
+}
