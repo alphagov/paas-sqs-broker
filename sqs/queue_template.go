33cfe34786695b70af556c25b0f74613fb24c21b
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-09-09 16:59:59 +0100

    remove unused

--- sqs/queue_template.go
+++ sqs/queue_template.go
@@ -14,7 +14,6 @@ const (
 	SQSQueueURLOutputName   = "QueueURL"
 	SQSDLQueueURLOutputName = "DLQueueURL"
 	SQSResourceIAMPolicy    = "SQSSIAMPolicy"
-	IAMRoleParameterName    = "IAMRoleName"
 	SQSQueueARNOutputName   = "QueueARN"
 	SQSDLQueueARNOutputName = "DLQueueARN"
 	SQSRegionOutputName     = "Region"
